cc82fca45874d8197233062b71cbae45a7c8cc51
Aaron Bond <aaron.bond@outlook.com>
2017-07-12 01:03:04 +0100

    refactor and adding ability to highlight multiline comments

--- extension.ts
+++ extension.ts
@@ -4,28 +4,27 @@ import * as vscode from 'vscode';
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
-	const contributions = vscode.workspace.getConfiguration('better-comments');
+	let activeEditor: any;
 
+	const contributions = vscode.workspace.getConfiguration('better-comments');
 	const alert_decorationType = vscode.window.createTextEditorDecorationType({ color: contributions.alertColor });
 	const question_decorationType = vscode.window.createTextEditorDecorationType({ color: contributions.questionColor });
 	const removed_decorationType = vscode.window.createTextEditorDecorationType({ color: contributions.removedColor, textDecoration: 'line-through' });
 	const todo_decorationType = vscode.window.createTextEditorDecorationType({ color: contributions.todoColor });
 	const highlight_decorationType = vscode.window.createTextEditorDecorationType({ color: contributions.highlightColor });
+	const showMultilineHighlight: boolean = contributions.multilineComments;
 
-	function updateDecorations() {
-		if (!activeEditor) return;
+	let alerts: vscode.DecorationOptions[] = [];
+	let questions: vscode.DecorationOptions[] = [];
+	let informational: vscode.DecorationOptions[] = [];
+	let removed: vscode.DecorationOptions[] = [];
+	let todos: vscode.DecorationOptions[] = [];
+	let highlights: vscode.DecorationOptions[] = [];
 
+	let findSingleLineComments = function () {
 		// Regex will find: // + ! OR ? OR // OR TODO, until end of line
-		const regEx = /(\/\/)+( )?(\!|\?|\/\/|\*|[\\t\\T][\\o\\O][\\d\\D][\\o\\O])+(.*)+/g;
-
+		const regEx = /(\/\/)+( )?(\!|\?|\/\/|\*|(todo))+(.*)+/ig;
 		const text = activeEditor.document.getText();
-
-		const alerts: vscode.DecorationOptions[] = [];
-		const questions: vscode.DecorationOptions[] = [];
-		const informational: vscode.DecorationOptions[] = [];
-		const removed: vscode.DecorationOptions[] = [];
-		const todos: vscode.DecorationOptions[] = [];
-		const highlights: vscode.DecorationOptions[] = [];
 
 		let match;
 		while (match = regEx.exec(text)) {
@@ -56,6 +55,60 @@ export function activate(context: vscode.ExtensionContext) {
 					break;
 			}
 		}
+	};
+
+	let findMultilineComments = function () {
+		const regEx = /(^|[ \t])(\/\*)+([\s\S]*?)(\*\/)/g;
+		const text = activeEditor.document.getText();
+		const commentRegEx = /(^)+([ \t]*)?(\!|\?|\*|(todo))+( )?([a-z])+([^*/\r\n]*)/igm;
+
+		let match;
+		while (match = regEx.exec(text)) {
+			let commentBlock = match[0];
+			let line;
+			while (line = commentRegEx.exec(commentBlock)) {
+				const startPos = activeEditor.document.positionAt(match.index + line.index);
+				const endPos = activeEditor.document.positionAt(match.index + line.index + line[0].length);
+				const decoration = { range: new vscode.Range(startPos, endPos) };
+
+				let matchString = line[3] as string;
+				switch (matchString.toLowerCase()) {
+					case "!":
+						alerts.push(decoration);
+						break;
+
+					case "?":
+						questions.push(decoration);
+						break;
+
+					case "todo":
+						todos.push(decoration);
+						break;
+
+					case "*":
+						highlights.push(decoration);
+						break;
+				}
+			}
+		}
+	};
+
+	let updateDecorations = function () {
+		if (!activeEditor) return;
+
+		// Make sure the decoration options are currently clear
+		alerts.length = 0;
+		questions.length = 0;
+		informational.length = 0;
+		removed.length = 0;
+		todos.length = 0;
+		highlights.length = 0;
+
+		findSingleLineComments();
+
+		if (showMultilineHighlight) {
+			findMultilineComments();
+		}
 
 		activeEditor.setDecorations(alert_decorationType, alerts);
 		activeEditor.setDecorations(question_decorationType, questions);
@@ -65,7 +118,7 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	// Text update handlers
-	let activeEditor = vscode.window.activeTextEditor;
+	activeEditor = vscode.window.activeTextEditor;
 	if (activeEditor) {
 		triggerUpdateDecorations();
 	}
@@ -92,4 +145,4 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 }
 
-export function deactivate() {}
+export function deactivate() { }
