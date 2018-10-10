c6a619c841857f9a0fc400a596d4f86bf980fe29
aaron-bond <aaron-bond@users.noreply.github.com>
2018-10-10 20:27:16 +0100

    Adding D language support, closes #99

---
+++ src/test/samples/d.d
@@ -0,0 +1,17 @@
+//version(none)  // ! this is the original version line
+version(all)     // ? the code is now enabled
+void brokenFunc() {
+    
+	/* 
+	
+	! comment freely here 
+	
+	*/
+	
+	int x=123;  
+    int y=321;  /+ to your heart's content +/
+}
+
+void workingFunc() {
+    
+}
