5966e4546618b38300004835007b827e3f4946c9
Dario <38925212+darltrash@users.noreply.github.com>
2020-06-13 19:10:10 -0400

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -334,6 +334,10 @@ export class Parser {
 			case "lua":
 				this.setCommentFormat("--", "--[[", "]]");
 				break;
+				
+			case "moonscript":
+				this.setCommentFormat("--", "--[[", "]]");
+				break;
 
 			case "elm":
 			case "haskell":
